452b1bc5bd9827227b5e867330ad82516ee67e2b
Kostiantyn Smyrnov <kostysh@gmail.com>
2018-04-23 23:10:43 +0300

    travis config update

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-echo "$SERVER_PASSWORD" | sudo -S su
 cd /home/orlovsky/pyrrha
-git reset --hard
-git pull
+sudo git reset --hard
+sudo git pull
 cd /home/orlovsky/pyrrha/containers
-docker-compose pull
-docker-compose down
-docker-compose up -d
+sudo docker-compose pull
+sudo docker-compose down
+sudo docker-compose up -d
