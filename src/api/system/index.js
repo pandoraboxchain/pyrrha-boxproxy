c1543c8f3b2de00efead9179de2fc88299f1e880
Kostiantyn Smyrnov <kostysh@gmail.com>
2018-05-24 16:01:58 +0300

    /system/addresses endpoint

--- src/api/system/index.js
+++ src/api/system/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controller = require('./system.controller');
 
 router.get('/version', controller.getVersion);
+router.get('/addresses', controller.getAddresses);
 
 module.exports = router;
