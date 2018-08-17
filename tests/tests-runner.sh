6d48dc798b4aa73b57bf33acf75443d7e19c4d81
Kostiantyn Smyrnov <kostysh@gmail.com>
2018-08-17 13:42:22 +0300

    travis script update

--- tests/tests-runner.sh
+++ tests/tests-runner.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+# Exit script as soon as a command fails.
+set -o errexit
+
 if [ "$WITH_COVERAGE" -eq 0 ]; then 
     echo "Running tests without coverage"
     npx mocha --exit --require @babel/register -R spec --timeout 70000 ./tests/spec/**/*.test.js #./tests/spec/subscriptionsManager.test.js
 
 else 
     echo "Running tests with coverage"
-    npx istanbul cover _mocha --exit --report lcovonly -- --require @babel/register -R spec --timeout 70000 ./tests/spec/**/*.test.js
+    npx istanbul cover _mocha --report lcovonly --  --exit --require @babel/register -R spec --timeout 70000 ./tests/spec/**/*.test.js
     cat ./coverage/lcov.info | npx codacy-coverage --token ecb18a8ad15a42498b68baf016654812
 fi
